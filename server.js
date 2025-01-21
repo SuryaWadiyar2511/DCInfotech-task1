4fa16ffb7cf310f1c0621eab2a5d39ee6e0f98fb
SuryaWadiyar2511 <suryawadiyar@gmail.com>
2025-01-21 23:01:17 +0530

    backend-Task1-DCinfotech

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/dbConfig');
-const feedbackRoutes = require('./routes/feedbackRoutes');
-const userRoutes = require('./routes/userRoutes');
-
-dotenv.config();
-connectDB();
+const authRoutes = require('./routes/auth');
 
 const app = express();
+
+// Middleware
+app.use(express.static(Path2D.join(--__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(Path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
-app.use('/api/feedback', feedbackRoutes);
-app.use('/api/users', userRoutes);
+// Routes
+app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+// Database Connection
+mongoose.connect('mongodb://127.0.0.1:27017/feedbackDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once('open', () => console.log('Connected to MongoDB!'));
+
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
